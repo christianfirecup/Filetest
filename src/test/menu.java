b0bc24b239c183d42ab23f40b4e775b6af2279b8
Christianfirecup <Christianfirecup@aol.com>
2022-09-21 22:57:27 -0400

    new code

--- src/test/menu.java
+++ src/test/menu.java
@@ -1,9 +1,9 @@
 package test;
-
+import test.sworditem;
 public class menu {
 
     public static void menutest() {
         System.out.println("Menu Options:");
-
+        sworditem.oncall();
     }
 }
--- src/test/sworditem.java
+++ src/test/sworditem.java
@@ -3,19 +3,31 @@ import  test.item;
 import java.util.Random;
 public class sworditem extends item {
     public String Rdisplay;
+    public static final String ANSI_YELLOW = "\\u001B[47m";
     public int rareselect = 0;
+    public static void oncall() {
+        sworditem call = new sworditem();
+
+        call.damage();
+        call.Rareity();
+        call.itemdesc();
+    }
+
    public Random rand = new Random();
     @Override
     public int damage(){
         return 4;
     }
-    public static void tiemdesc(){
+    public void itemdesc(){
+        if(rareselect == 0){
+            System.out.println(  "ITEM STAT: Common");
 
+        }
         }
         @Override
         public int Rareity(){
           int Rareityset = rand.nextInt(4);
-          rareselect = Rareityset;
+          rareselect = 0;
             return Rareityset;
         }
 
